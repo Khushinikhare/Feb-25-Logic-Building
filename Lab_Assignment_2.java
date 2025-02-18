094d59342078dbdcd3085d8c79ecf1b173028ddc
Khushi <96741591+Khushinikhare@users.noreply.github.com>
2025-02-18 21:34:02 +0530

    Add files via upload

---
+++ Lab_Assignment_2.java
@@ -0,0 +1,9 @@
+public class Main {
+ public void main(String[] args) {
+ System.out.println("Hello, World!");
+ }
+}
+/* 
+Snippet 1:
+Error: class Main is public, should be declared in a file named Main.java
+*/
