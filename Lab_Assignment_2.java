437a5b3d56fe3f8ef45b9c4198db110cb1cbe522
Khushi <96741591+Khushinikhare@users.noreply.github.com>
2025-02-19 15:13:28 +0530

    Delete Lab_Assignment_2.java
    Submitted incomplete assignment

--- Lab_Assignment_2.java
+++
@@ -1,9 +0,0 @@
-public class Main {
- public void main(String[] args) {
- System.out.println("Hello, World!");
- }
-}
-/* 
-Snippet 1:
-Error: class Main is public, should be declared in a file named Main.java
-*/
